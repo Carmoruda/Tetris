90bf44a94ea6a485b36402e329003477bc087570
Carmen <carmen.moreno.udaondo@gmail.com>
2023-12-08 12:50:00 +0100

    GameScreen function and variables comments

--- src/Screens/GameScreen.asm
+++ src/Screens/GameScreen.asm
@@ -4,31 +4,31 @@
 ;-----------------------------------------------------------------------------------------
 GAMESCREEN:
     CALL CLEARSCR   ; Clean screen.
-    CALL TETRIS_3D
-    LD B, 1     ; Square Row
+    CALL TETRIS_3D  ; Paint the 3D Tetris effect
+    LD B, 1         ; Row
 ;-----------------------------------------------------------------------------------------
 
 ;-----------------------------------------------------------------------------------------
 ; VERTICAL_BORDER - Display the U vertical sides.
 ;-----------------------------------------------------------------------------------------
 VERTICAL_BORDER:
-    LD C, 6     ; Square Column
+    LD C, 6     ; Column
     LD A, $38   ; Square color (hex) -> White
     CALL DOTYXC ; Paint square
 
     PUSH AF
-    LD A, C
-    ADD TETRIS_WIDTH
-    LD C, A
+    LD A, C          ; A = Column
+    ADD TETRIS_WIDTH ; A = Column + TETRIS_WIDTH
+    LD C, A          ; C = Column + TETRIS_WIDTH
     POP AF
-    CALL DOTYXC
 
-    LD A, B
-    INC B
+    CALL DOTYXC            ; Paint square
+    LD A, B                ; A = Row
+    INC B                  ; B = Row + 1
     CP TETRIS_HEIGHT
-    JR NZ, VERTICAL_BORDER
+    JR NZ, VERTICAL_BORDER ; If Row < TETRIS_HEIGHT, repeat
 
-    LD C, 6     ; Square Column
+    LD C, 6     ; Column
 ;-----------------------------------------------------------------------------------------
 
 ;-----------------------------------------------------------------------------------------
@@ -38,14 +38,17 @@ HORIZONTAL_BORDER:
     LD A, $38   ; Square color (hex) -> White
     CALL DOTYXC ; Paint square
 
-    LD A, C
-    INC C
+    LD A, C                  ; A = Column
+    INC C                    ; C = Column + 1
     CP TETRIS_MAX_WIDTH
-    JR NZ, HORIZONTAL_BORDER
+    JR NZ, HORIZONTAL_BORDER ; If Column < TETRIS_MAX_WIDTH, repeat
 ;-----------------------------------------------------------------------------------------
 
+;-----------------------------------------------------------------------------------------
+; GAME_TETROMINO - Paints a random tetromino in the game screen (row 1, column 15).
+;-----------------------------------------------------------------------------------------
 GAME_TETROMINO:
-    CALL RANDOM_NUMBER
+    CALL RANDOM_NUMBER ; Returns a random tetromino in the IX register.
 
     LD A, 1         ; Screen row
     LD (ROWS), A    ; Save row
@@ -53,35 +56,40 @@ GAME_TETROMINO:
     LD A, 15        ; Screen column
     LD (COLUMNS), A ; Save column
 
-    LD A, (IX)
-    LD (PIECE_HEIGHT), A
+    LD A, (IX)           ; Tetromino height
+    LD (PIECE_HEIGHT), A ; Save tetromino height
 
-    CALL PAINT_TETROMINO
+    CALL PAINT_TETROMINO ; Paint tetromino
+;-----------------------------------------------------------------------------------------
 
+;-----------------------------------------------------------------------------------------
+; MOVE_TETROMINO_DOWN - Moves the tetromino down until it reaches the bottom border.
+;-----------------------------------------------------------------------------------------
 MOVE_TETROMINO_DOWN:
-    CALL ERASE_TETROMINO
-    LD A, (ROWS)
-    INC A
-    LD (ROWS), A
+    CALL ERASE_TETROMINO ; Erase tetromino
+    LD A, (ROWS)         ; A = Rows
+    INC A                ; A = Rows + 1
+    LD (ROWS), A         ; Save Rows
+
     PUSH AF
-    CALL PAINT_TETROMINO
-
-    CALL DELAY
-    LD A, (PIECE_HEIGHT)
-    LD B, A
-    LD A, 21
-    SUB B
-    LD B, A
+    CALL PAINT_TETROMINO ; Paint tetromino
+    CALL DELAY           ; Time delay
+    LD A, (PIECE_HEIGHT) ; A = Tetromino height
+    LD B, A              ; B = Tetromino height
+    LD A, 21             ; A = 21
+    SUB B                ; A = 21 - Tetromino height
+    LD B, A              ; B = 21 - Tetromino height
     POP AF
-    CP B ; 21 - RowsTetromino
-    JR NZ, MOVE_TETROMINO_DOWN
 
-END_MOVE_TETROMINO_DOWN:
-    CALL ERASE_TETROMINO
-    LD A, (ROWS)
-    INC A
-    LD (ROWS), A
-    CALL PAINT_TETROMINO
+    CP B
+    JR NZ, MOVE_TETROMINO_DOWN ; If A != B, repeat
+
+    CALL ERASE_TETROMINO ; Erase tetromino
+    LD A, (ROWS)         ; A = Rows
+    INC A                ; A = Rows + 1
+    LD (ROWS), A         ; Save Rows
+    CALL PAINT_TETROMINO ; Paint tetromino
+;-----------------------------------------------------------------------------------------
 
 ;-----------------------------------------------------------------------------------------
 ; GAMELOOP - Game simulation.
@@ -90,22 +98,22 @@ GAMELOOP:
     JP GAME_TETROMINO
 ;-----------------------------------------------------------------------------------------
 
+;-----------------------------------------------------------------------------------------
+; DELAY - Time delay.
+;-----------------------------------------------------------------------------------------
 DELAY:
     LD HL, 10000
 DELAY_LOOP:
-    DEC HL
-    LD A, H
+    DEC HL            ; HL = HL--
+    LD A, H           ; A = H
     OR 0
-    JR NZ, DELAY_LOOP
+    JR NZ, DELAY_LOOP ; If HL != 0, repeat
     RET
+;-----------------------------------------------------------------------------------------
 
-TETRIS_WIDTH EQU 19
-TETRIS_MAX_WIDTH EQU 25
-TETRIS_HEIGHT EQU 21
-PIECE_HEIGHT: DB 0
+; -------- VARIABLES -------
+TETRIS_WIDTH EQU 19     ; Space between the U borders.
+TETRIS_MAX_WIDTH EQU 25 ; Last column of the U borders.
+TETRIS_HEIGHT EQU 21   ; Last row of the U borders.
+PIECE_HEIGHT: DB 0     ; Tetromino height.
 GAMEMESSAGE: DB "GAME", 0
-
-; CALL DIBUJA
-; ESPERA (El tiempo de espera debe ser variable)
-; BORRAMOS LA FORMA
-; SUMAMOS 1 A LA FILA
