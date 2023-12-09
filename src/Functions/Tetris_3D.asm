3781a35f129ce090184a81c4332804176bead75a
Carmen <carmen.moreno.udaondo@gmail.com>
2023-12-09 10:09:11 +0100

    Tetris_3D and paint tetrominos comments

--- src/Functions/Tetris_3D.asm
+++ src/Functions/Tetris_3D.asm
@@ -1,27 +1,70 @@
-Tetro_3D:   DB $FF, $81, $81, $85, $85, $9D, $81, $FF ; Patrón para los bloques
+Tetro_3D:   DB $FF, $81, $81, $85, $85, $9D, $81, $FF ; Block pattern
 
+;-----------------------------------------------------------------------------------------
+; TETRIS_3D - Paint the 3D Tetris effect.
+; OUT - D: Number of pixel blocks to paint.
+;       IX: VRAM address.
+;-----------------------------------------------------------------------------------------
 TETRIS_3D:
-    LD D,3              ; Los 3 bloques de píxeles
-    LD IX,$4000         ; Dirección de comienzo de la VRAM
+    LD D, 3              ; 3 blocks of pixels
+    LD IX, $4000         ; VRAM starting address
+;-----------------------------------------------------------------------------------------
 
+
+;-----------------------------------------------------------------------------------------
+; T3D3 - Set first byte and number of bytes of the pattern.
+; IN - D: Number of pixel blocks to paint.
+;      IX: VRAM address.
+; OUT - IX: VRAM address.
+;       IY: First byte of the pattern.
+;       C: Number of bytes of the pattern.
+;       D: Number of pixel blocks to paint.
+;-----------------------------------------------------------------------------------------
 T3D3:
+    LD IY, Tetro_3D      ; First byte of the pattern
+    LD C, 8              ; 8 bytes of the pattern
+;-----------------------------------------------------------------------------------------
 
-    LD IY,Tetro_3D      ; Primer byte del patrón
 
-    LD C,8              ; 8 bytes del patrón
+;-----------------------------------------------------------------------------------------
+; T3D2 - Set first byte and number of bytes of the pattern.
+; IN - IX: VRAM address.
+;      IY: First byte of the pattern.
+;      C: Number of bytes of the pattern.
+;      D: Number of pixel blocks to paint.
+; OUT - IX: VRAM address.
+;       IY: First byte of the pattern.
+;       A: Pattern value.
+;       B: Number of bytes for each line of the pattern.
+;       C: Number of bytes of the pattern.
+;       D: Number of pixel blocks to paint.
+;-----------------------------------------------------------------------------------------
 T3D2:
-    LD B,0              ; 256 bytes de cada línea del patrón
-    LD a,(IY)           ; Cargo el valor del patrón
+    LD B, 0              ; 256 bytes for each line of the pattern
+    LD A, (IY)           ; Load pattern value
+;-----------------------------------------------------------------------------------------
+
+
+;-----------------------------------------------------------------------------------------
+; T3D1 - Paint the pattern.
+; IN - IX: VRAM address.
+;      IY: First byte of the pattern.
+;      A: Pattern value.
+;      B: Number of bytes for each line of the pattern.
+;      C: Number of bytes of the pattern.
+;      D: Number of pixel blocks to paint.
+;-----------------------------------------------------------------------------------------
 T3D1:
-    LD (IX),a           ; Cargo el patrón en la VideoRam
-    INC IX              ; Siguientes 8 pixels
-    DJNZ T3D1           ; Fin del bucle
+    LD (IX), A          ; Load the pattern into the VideoRam
+    INC IX              ; Next 8 pixels
+    DJNZ T3D1           ; End of the 1st loop
 
-    DEC C               ; Fin del 2º Bucle
-    inc IY              ; Siguiente byte del patrón
-    JR NZ,T3D2
+    DEC C               ; End of the 2nd loop
+    INC IY              ; Next byte of the pattern
+    JR NZ, T3D2
 
-    DEC D               ; Fin del 3º bucle
+    DEC D               ; End of the 3rd loop
     JR NZ, T3D3
 
     RET
+;-----------------------------------------------------------------------------------------
--- src/Tetrominos/PaintTetrominos.asm
+++ src/Tetrominos/PaintTetrominos.asm
@@ -3,6 +3,10 @@
 ;	  IN -  IX = Tetromino we want to paint.
 ;           ROWS = Row of the screen in which we want to paint.
 ;           COLUMNS = Column of the screen in which we want to paint.
+;     OUT - IX = Tetromino we want to paint.
+;           IY = Pointer to the first square of the tetromino.
+;           B = Row of the screen in which we want to paint.
+;           E = Number of rows.
 ;-----------------------------------------------------------------------------------------
 PAINT_TETROMINO:
     LD IY, IX       ; IY = Tetromino we want to paint
@@ -19,6 +23,12 @@ PAINT_TETROMINO:
 ;           IY = Pointer to the tetromino square we want to paint.
 ;           B = Row of the screen in which we want to paint.
 ;           E = Number of rows.
+;     OUT - IX = Tetromino we want to paint.
+;           IY = Pointer to the first square of the tetromino.
+;           B = Row of the screen in which we want to paint.
+;           C = Column of the screen in which we want to paint.
+;           D = Number of columns.
+;           E = Number of rows.
 ;-----------------------------------------------------------------------------------------
 PAINT_TETROMINO_OUTERLOOP:
     LD D, (IX + 1)  ; Number of columns
@@ -29,8 +39,17 @@ PAINT_TETROMINO_OUTERLOOP:
 
 ;-----------------------------------------------------------------------------------------
 ; PAINT_TETROMINO_INNERRLOOP - Loops through the columns of a row and erases the squares.
-;	  IN -  IY = Pointer to the tetromino square we want to paint
+;	  IN -  IX = Tetromino we want to paint.
+;           IY = Pointer to the first square of the tetromino.
 ;           B = Row of the screen in which we want to paint.
+;           C = Column of the screen in which we want to paint.
+;           D = Number of columns.
+;           E = Number of rows.
+;	  OUT - IX = Tetromino we want to paint.
+;           IY = Pointer to the next square of the tetromino.
+;           A = Color of the square.
+;           B = Row of the screen in which we want to paint.
+;           C = Column of the screen in which we want to paint.
 ;           D = Number of columns.
 ;           E = Number of rows.
 ;-----------------------------------------------------------------------------------------
@@ -44,20 +63,55 @@ PAINT_TETROMINO_INNERLOOP:
 ;-----------------------------------------------------------------------------------------
 
 
+;-----------------------------------------------------------------------------------------
+; PAINT_TETROMINO_LOOP - Paints a square.
+;	  IN - IX = Tetromino we want to paint.
+;          IY = Pointer to the next square of the tetromino.
+;          A = Color of the square.
+;          B = Row of the screen in which we want to paint.
+;          C = Column of the screen in which we want to paint.
+;          D = Number of columns.
+;          E = Number of rows.
+;	  OUT - IX = Tetromino we want to paint.
+;           IY = Pointer to the next square of the tetromino.
+;           A = Color of the square.
+;           B = Row of the screen in which we want to paint.
+;           C = Next column of the screen in which we want to paint.
+;           D = Number of columns.
+;           E = Number of rows.
+;-----------------------------------------------------------------------------------------
 PAINT_TETROMINO_LOOP:
     PUSH DE
     CALL DOTYXC         ; Paint square
     POP DE
     INC C               ; Next column
+;-----------------------------------------------------------------------------------------
+
+
+;-----------------------------------------------------------------------------------------
+; PAINT_TETROMINO_CHECK_LOOPS - Checks if we have to continue looping.
+;    IN - IX = Tetromino we want to paint.
+;         IY = Pointer to the next square of the tetromino.
+;         B = Row of the screen in which we want to paint.
+;         C = Column of the screen in which we want to paint.
+;         D = Number of columns.
+;         E = Number of rows.
+;   OUT - IX = Tetromino we want to paint.
+;         IY = Pointer to the next square of the tetromino.
+;         C = Column of the screen in which we want to paint.
+;         B = Next row of the screen in which we want to paint.
+;         D = Number of columns.
+;         E = Number of rows.
+;-----------------------------------------------------------------------------------------
 PAINT_TETROMINO_CHECK_LOOPS:
     LD A, D                             ; A = D
     CP 0                                ; Column = 0?
-    DEC D                               ; Column -= 1
+    DEC D                               ; Column--
     JP NZ, PAINT_TETROMINO_INNERLOOP    ; Yes - Paint
     INC B                               ; Next row
     LD A, E                             ; A = E
     CP 0                                ; Row = 0?
-    DEC E                               ; Row -= 1
+    DEC E                               ; Row--
     JP NZ, PAINT_TETROMINO_OUTERLOOP    ; No - Loop
     RET
 ;-----------------------------------------------------------------------------------------
